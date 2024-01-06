89bd677ea2b2d02402910eca8426a5bd8e7f20ff
hooktaunttt <153667568+hooktaunttt@users.noreply.github.com>
2024-01-06 16:03:58 +0300

    Add files via upload

---
+++ responsive-sidebar-menu/js/main.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+  let navigation = document.querySelector('.navigation');
+  let toggle = document.querySelector('.toggle');
+  navigation.classList.toggle('active');
+  toggle.classList.toggle('active');
+}
+
+
+
+
